2f9e758f08d21ca411b571781e0bf2c229455daf
Anto-dot <antonellaromero192@gmail.com>
2026-06-01 13:14:54 -0300

    Update loding.js

--- js/loding.js
+++ js/loding.js
@@ -1,1 +1,17 @@
+const carrito =
+JSON.parse(localStorage.getItem("carrito")) || [];
 
+const contenedor =
+document.getElementById("carrito");
+
+carrito.forEach(producto => {
+
+    contenedor.innerHTML += `
+    <div class="card">
+        <img src="${producto.imagen}">
+        <h3>${producto.titulo}</h3>
+        <p>${producto.descripcion}</p>
+        <h4>$${producto.precio}</h4>
+    </div>
+    `;
+});
