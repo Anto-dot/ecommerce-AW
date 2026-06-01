72df79595ebadad077ed490143d60f65ba99b46e
Anto-dot <antonellaromero192@gmail.com>
2026-06-01 13:12:51 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+const contenedor = document.getElementById("productos");
 
+fetch("./data/productos.json")
+.then(response => response.json())
+.then(productos => {
+
+    productos.forEach(producto => {
+
+        contenedor.innerHTML += `
+        <div class="card">
+            <img src="${producto.imagen}">
+            <h3>${producto.titulo}</h3>
+            <p>${producto.descripcion}</p>
+            <h4>$${producto.precio}</h4>
+
+            <button onclick="agregarCarrito(${producto.id})">
+                Agregar al carrito
+            </button>
+        </div>
+        `;
+    });
+
+    window.listaProductos = productos;
+});
